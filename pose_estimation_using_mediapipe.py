263a340076e09f42cd3b4513059419109a129c76
gamal ahmed <gamalahmed3265@gmail.com>
2023-01-30 18:30:00 +0200

    add pose_estimation_using_mediapipe.py

--- pose_estimation_using_mediapipe.py
+++ pose_estimation_using_mediapipe.py
@@ -13,24 +13,6 @@ pose=mp_pose.Pose(
 
 
 
-def processPose(image):
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pose.process(image)
-
-    # Draw the pose annotation on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    mp_drawing.draw_landmarks(
-        image,
-        results.pose_landmarks,
-        mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    # Flip the image horizontally for a selfie-view display.
-
-
 # For webcam input:
 cap = cv2.VideoCapture(0)
 
