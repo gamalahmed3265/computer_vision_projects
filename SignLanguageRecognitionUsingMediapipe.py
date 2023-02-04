766a32e0b5c3fbc7db2ed7d629fc760bcc6f1442
gamal ahmed <gamalahmed3265@gmail.com>
2023-02-04 21:42:50 +0200

    add SignLanguageRecognitionUsingMediapipe.py part 2

--- SignLanguageRecognitionUsingMediapipe.py
+++ SignLanguageRecognitionUsingMediapipe.py
@@ -17,7 +17,13 @@ cap.set(4,hCam)
 
 font=cv.FONT_HERSHEY_PLAIN
 
+
 def SignLanguageRecognitionUsingMediapipe(image,lst):
+    
+    def putText(text):
+        cv.putText(image,text,(40,100),font,2,(255,0,0),3)
+
+    fingerFoldStatus=[]
     fingerTips=[8,12,16,20]
     thumb=4
     
@@ -25,7 +31,7 @@ def SignLanguageRecognitionUsingMediapipe(image,lst):
     # [12, 344, 609]
     # [16, 286, 630]
     # [20, 221, 648]
-    
+    cv.circle(image,(lst[thumb][1],lst[thumb][2]),15,(0,0,255),cv.FILLED)
     for fin in fingerTips:
         # print(lst[fin])
         cv.circle(image,(lst[fin][1],lst[fin][2]),15,(255,0,0),cv.FILLED)
@@ -33,11 +39,45 @@ def SignLanguageRecognitionUsingMediapipe(image,lst):
         #   8           5
         if lst[fin][1] < lst[fin-3][1]:
             # cv.circle(image,(lst[fin][1],lst[fin][2]),15,(0,0,255),cv.FILLED)
-            cv.putText(image,"Dislike",(40,100),font,2,(255,0,0),3)
+            fingerFoldStatus.append(True)
         else:
-            cv.putText(image,"like",(40,100),font,2,(255,0,0),3)
+            fingerFoldStatus.append(False)
+            
+            
+    if lst[4][2] < lst[2][2] and lst[8][2] < lst[6][2] and lst[12][2] < lst[10][2] and \
+                    lst[16][2] < lst[14][2] and lst[20][2] < lst[18][2] and lst[17][1] < lst[0][1] < \
+                    lst[5][1]:
+        putText("STOP")
+        print("STOP")
 
+    # Forward
+    if lst[3][1] > lst[4][1] and lst[8][2] < lst[6][2] and lst[12][2] > lst[10][2] and \
+            lst[16][2] > lst[14][2] and lst[20][2] > lst[18][2]:
+        putText("FORWARD")
+        print("FORWARD")
 
+    # Backward
+    if lst[3][1] > lst[4][1] and lst[3][2] < lst[4][2] and lst[8][2] > lst[6][2] and lst[12][2] < lst[10][2] and \
+            lst[16][2] < lst[14][2] and lst[20][2] < lst[18][2]:
+        putText("BACKWARD")
+        print("BACKWARD")
+
+    # Left
+    if lst[4][2] < lst[2][2] and lst[8][1] < lst[6][1] and lst[12][1] > lst[10][1] and \
+            lst[16][1] > lst[14][1] and lst[20][1] > lst[18][1] and lst[5][1] < lst[0][1]:
+        putText("LEFT")
+        print("LEFT")
+
+    # Right
+    if lst[4][2] < lst[2][2] and lst[8][1] > lst[6][1] and lst[12][1] < lst[10][1] and \
+            lst[16][1] < lst[14][1] and lst[20][1] < lst[18][1]:
+        cv.putText(image, "RIGHT", (20, 30),font, 1, (0, 0, 255), 3)
+        print("RIGHT")
+    if all(fingerFoldStatus):
+        if lst[thumb][2] < lst[thumb-1][2] <lst[thumb-2][2]:
+            putText("like")
+        if lst[thumb][2] > lst[thumb-1][2] >lst[thumb-2][2]:
+              putText("Dislike")
 def main():
     font=cv.FONT_HERSHEY_PLAIN
     pTime=0
@@ -52,7 +92,7 @@ def main():
             
             if len(lmList) != 0:
                 SignLanguageRecognitionUsingMediapipe(frame,lmList)
-            
+              
             cTime=time.time()
             fbs=1/(cTime-pTime)
             pTime=cTime
